51e953a45d904d07d84e7fe539b036ca8b0be9cf
Robin Ladenburger <ladenburger.robin57@gmail.com>
2020-07-24 16:11:15 +0200

    a

--- service.js
+++ service.js
@@ -2,33 +2,41 @@ const request = require('request');
 
 function getClients(callback) {
     request('https://robin-hotel-web-api.herokuapp.com/clients/all', { json: true }, function (err, res, body) {
-        if (err) { return console.log('Erreur', err); }
-        callback(body)
-
+        if (err) {
+            callback('Erreur', err);
+        } else {
+            callback(body)
+        }
     });
 }
 
-function addClient(callback, nom , prenom){
+function addClient(callback, nom, prenom) {
     request.post({
-        headers: {'content-type' : 'application/json'},
-        url:     'https://robin-hotel-web-api.herokuapp.com/clients',
-        body:    JSON.stringify({ nom: nom, prenoms:prenom })
-      }, function(error, response, body){
-        callback(body)
-      });
-}
-
-function rechercheParNom(callback, nom){
-    request('https://robin-hotel-web-api.herokuapp.com/clients/search/' + nom, { json: true }, function (err, res, body) {
-        if (err) { return console.log('Erreur', err); }
+        headers: { 'content-type': 'application/json' },
+        url: 'https://robin-hotel-web-api.herokuapp.com/clients',
+        body: JSON.stringify({ nom: nom, prenoms: prenom })
+    }, function (error, response, body) {
         callback(body)
     });
 }
 
-function verifieChambre(callback, numero){
+function rechercheParNom(callback, nom) {
+    request('https://robin-hotel-web-api.herokuapp.com/clients/search/' + nom, { json: true }, function (err, res, body) {
+        if (err) {
+            callback('Erreur', err);
+        } else {
+            callback(body)
+        }
+    });
+}
+
+function verifieChambre(callback, numero) {
     request('https://robin-hotel-web-api.herokuapp.com/reservations/chambre/' + numero, { json: true }, function (err, res, body) {
-        if (err) { return console.log('Erreur', err); }
-        callback(body)
+        if (err) {
+            callback('Erreur', err);
+        } else {
+            callback(body)
+        }
     });
 }
 
